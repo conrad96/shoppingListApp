b15c159486484ac3984935f614da4b95e77be0ca
conrad96 <conrad.mugisha@gmail.com>
2020-01-05 23:17:52 +0300

    added bootstrap, refactored mainwindow code

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const electron = require('electron');
-
+const {app, BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
-
-let mainWindow;
-
-app.on('ready', function(){
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
+let window;
+function mainWindow(){
+    window = new BrowserWindow({});
+    window.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
         slashes: true
     }));
+}
+//listen for app to be ready
+app.on('ready', mainWindow);
+//close when all windows are closed
+app.on('window-all-closed', ()=>{
+    window = null;
+    app.quit();
 });
